ddd45744ad83902bb145b8962b69b83d9dfa0e54
hadriengerard <hadriengerard@live.fr>
2014-06-11 14:19:57 +0200

    Webapp - Angular

--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -2,10 +2,57 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
+
+var ogcediApp = angular.module('ogcediApp', ['ngRoute', 'ogcediControllers', 'ogcediFilters', 'ogcediServices']);
+
+ogcediApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/personnes', {
+		templateUrl: 'partials/personne-list.html',
+		controller: 'PersonneListCtrl'
+	}).
+	when('/personnes/:personneId', 
+	{
+		templateUrl: 'partials/personne-detail.html',
+		controller: 'PersonneDetailCtrl'
+	}).
+	otherwise({
+		redirectTo: '/personnes'
+	});
+}]);
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------- 
+
+
+var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'phonecatControllers', 'phonecatFilters', 'phonecatServices']);
+
+phonecatApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/phones', {
+		templateUrl: 'partials/phone-list.html',
+		controller: 'PhoneListCtrl'
+	}).
+	when('/phones/:phoneId', 
+	{
+		templateUrl: 'partials/phone-detail.html',
+		controller: 'PhoneDetailCtrl'
+	}).
+	otherwise({
+		redirectTo: '/phones'
+	});
+}]);
+
+
+
+
+
+
+/*var phonecatApp = angular.module('phonecatApp', [
   'ngRoute',
   'phonecatAnimations',
-
+  
   'phonecatControllers',
   'phonecatFilters',
   'phonecatServices'
@@ -25,4 +72,4 @@ phonecatApp.config(['$routeProvider',
       otherwise({
         redirectTo: '/phones'
       });
-  }]);
+  }]);*/
